51aa6aad978721a3422322047608461d275613c2
prgm-hbo <programming.hbo@gmail.com>
2015-09-04 17:29:49 +0200

    -

---
+++ sandcastle/edge_sample/app.js
@@ -0,0 +1,8 @@
+var edge = require('edge');
+
+var hello = edge.func('async (input) => { return ".NET welcomes " + input.ToString(); }');
+
+hello('Node.js', function (error, result) {
+	if (error) throw error;
+	console.log(result);
+});
