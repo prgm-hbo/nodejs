e1d96fc221e55fbbbedf4fbebafc5c75a0ced76d
prgm-hbo <programming.hbo@gmail.com>
2015-09-07 17:11:18 +0200

    -

--- sandcastle/VS/Edge/Edge1/app.js
+++ sandcastle/VS/Edge/Edge1/app.js
@@ -1,13 +1,23 @@
 var edge = require('edge');
 
 // Call C# async lambda from Node.js
-var hello = edge.func(/*
-    async (input) => {
-        return ".NET welcomes " + input.ToString();
-    }*/
-);
+var test_1 = edge.func('async (p1) => { return ".NET call, param " + p1.ToString(); }');
 
-hello('Node.js', function (error, result) {
-	if (error) throw error;
-	console.log(result);
+var test_2 = edge.func(function () {/*
+	async (p1) => 
+	{ 
+		return ".NET call, param " + p1.ToString(); 
+	}
+*/
+});
+
+var p1 = "p1";
+test_1(p1, function (error, result) {
+  if (error) throw error;
+  console.log(result);
+});
+
+test_2(p1, function (error, result) {
+    if (error) throw error;
+    console.log(result);
 });
